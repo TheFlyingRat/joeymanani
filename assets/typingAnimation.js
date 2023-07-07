478719aafb733be9feee926686f90182ea4ae9b4
TheFlyingRat <joeyspp50@gmail.com>
2023-07-07 22:25:19 +1000

    added a delay after pressing enter after a quote

--- assets/typingAnimation.js
+++ assets/typingAnimation.js
@@ -1,25 +1,34 @@
 var charsTyped = 0;
 const typingCursor = document.getElementById("typing-cursor");
+let typingPaused = false;
 
 async function type(id, text, speed) {
-    const textPlaceholder = document.getElementById(id);
-    return new Promise(resolve => {
-        var id = setInterval(function() {
-            if (charsTyped < text.length) {
-                typingCursor.classList.remove("typing-cursor-animation");
-                if (text.charAt(charsTyped) === '\n') {
-                    textPlaceholder.innerHTML += '<br>';
-                } else {
-                    textPlaceholder.innerHTML += text.charAt(charsTyped);
-                }
-                playSound("button")
-                charsTyped++;
-            } else {
-                clearInterval(id);
-                typingCursor.classList.add("typing-cursor-animation");
-                resolve();
-            }
-        }, speed);
-        charsTyped = 0;
-    });
+  const textPlaceholder = document.getElementById(id);
+  return new Promise((resolve) => {
+    var intervalId = setInterval(function () {
+      if (!typingPaused && charsTyped < text.length) {
+        typingCursor.classList.remove("typing-cursor-animation");
+        if (text.charAt(charsTyped) === "\n") {
+          typingPaused = true;
+          typingCursor.classList.add("typing-cursor-animation");
+          setTimeout(() => {
+            playSound("enter");
+            typingCursor.classList.remove("typing-cursor-animation");
+            textPlaceholder.innerHTML += "<br>";
+            charsTyped++;
+            typingPaused = false;
+          }, 1000); // Delay of 30 milliseconds after typing the line break
+        } else {
+          playSound("button");
+          textPlaceholder.innerHTML += text.charAt(charsTyped);
+          charsTyped++;
+        }
+      } else if (charsTyped === text.length) {
+        clearInterval(intervalId);
+        typingCursor.classList.add("typing-cursor-animation");
+        resolve();
+      }
+    }, speed);
+    charsTyped = 0;
+  });
 }
