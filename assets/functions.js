b6564df7cd7becdd933851a0e66fc2250d77f63d
TheFlyingRat <joeyspp50@gmail.com>
2023-07-07 12:35:48 +1000

    audio cache to blob - no more http reqs (cdn sync)

--- assets/functions.js
+++ assets/functions.js
@@ -37,27 +37,54 @@ function getDateDifference(date) {
 const audioThread1 = new Audio();
 const audioThread2 = new Audio();
 
-// Play a sound when the button is selected or hovered
+// In-memory cache for the audio files
+const audioCache = {};
+
+// Preload the audio files into memory cache
+async function preloadAudio(sound) {
+  try {
+    const response = await fetch("https://cdn.theflyingrat.com/assets/joeymanani/assets/" + sound + ".wav");
+    const arrayBuffer = await response.arrayBuffer();
+    const blob = new Blob([arrayBuffer], { type: "audio/wav" });
+    const url = URL.createObjectURL(blob);
+    audioCache[sound] = url;
+  } catch (error) {
+    console.log("Error preloading audio: " + error);
+    }
+}
+
+// Preload all the audio files when the page loads
+window.addEventListener("load", function() {
+    preloadAudio("click");
+    preloadAudio("success");
+    preloadAudio("alert");
+    preloadAudio("enter");
+    preloadAudio("backspace");
+    preloadAudio("button");
+});
+
+// Play a sound from the in-memory cache
 function playSound(sound) {
     try {
         var playPromise;
         if (audioThread1.paused) {
-            audioThread1.src = "/assets/" + sound + ".wav";
+            audioThread1.src = audioCache[sound];
             playPromise = audioThread1.play();
         } else {
-            audioThread2.src = "/assets/" + sound + ".wav";
+            audioThread2.src = audioCache[sound];
             playPromise = audioThread2.play();
         }
-        
+
         if (playPromise !== undefined) {
             playPromise.catch(function(error) {
                 console.log("User didn't interact. Can't play sound!");
-            });
+        });
         }
     } catch (error) {
         console.log("Couldn't play sound because of error: " + error);
     }
 }
+
 // Sleep function
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
