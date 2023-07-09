fd65c9d88aff84ec44956e849d7a44dbca6b5312
TheFlyingRat <joeyspp50@gmail.com>
2023-07-09 18:30:53 +1000

    preload audio to localStorage + Blob #19

--- assets/functions.js
+++ assets/functions.js
@@ -37,52 +37,76 @@ function getDateDifference(date) {
 const audioThread1 = new Audio();
 const audioThread2 = new Audio();
 
-// In-memory cache for the audio files
-const audioCache = {};
-
-// Preload the audio files into memory cache
+// Preload the audio files into memory cache and store them in local storage
 async function preloadAudio(sound) {
   try {
     const response = await fetch("https://cdn.theflyingrat.com/assets/joeymanani/assets/" + sound + ".wav");
     const arrayBuffer = await response.arrayBuffer();
-    const blob = new Blob([arrayBuffer], { type: "audio/wav" });
-    const url = URL.createObjectURL(blob);
-    audioCache[sound] = url;
+    const base64Data = arrayBufferToBase64(arrayBuffer);
+    localStorage.setItem(sound, base64Data);
   } catch (error) {
     console.log("Error preloading audio: " + error);
-    }
+  }
 }
 
-// Preload all the audio files when the page loads
-window.addEventListener("load", function() {
-    preloadAudio("click");
-    preloadAudio("success");
-    preloadAudio("alert");
-    preloadAudio("enter");
-    preloadAudio("backspace");
-    preloadAudio("button");
+// Convert an ArrayBuffer to a Base64 string
+function arrayBufferToBase64(arrayBuffer) {
+  const bytes = new Uint8Array(arrayBuffer);
+  let binary = "";
+  for (let i = 0; i < bytes.length; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  return btoa(binary);
+}
+
+// Convert a Base64 string to an ArrayBuffer
+function base64ToArrayBuffer(base64) {
+  const binary = atob(base64);
+  const bytes = new Uint8Array(binary.length);
+  for (let i = 0; i < binary.length; i++) {
+    bytes[i] = binary.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
+
+// Preload all the audio files when the JS executes
+const audioFiles = ["click", "success", "alert", "enter", "backspace", "button"];
+audioFiles.forEach(sound => {
+  const cachedAudio = localStorage.getItem(sound);
+  if (!cachedAudio) {
+    preloadAudio(sound);
+  }
+});
+
+// Create Blob URLs for audio files
+const audioBlobURLs = {};
+audioFiles.forEach(sound => {
+  const base64Data = localStorage.getItem(sound);
+  const arrayBuffer = base64ToArrayBuffer(base64Data);
+  const audioBlob = new Blob([arrayBuffer], { type: "audio/wav" });
+  audioBlobURLs[sound] = URL.createObjectURL(audioBlob);
 });
 
 // Play a sound from the in-memory cache
 function playSound(sound) {
-    try {
-        var playPromise;
-        if (audioThread1.paused) {
-            audioThread1.src = audioCache[sound];
-            playPromise = audioThread1.play();
-        } else {
-            audioThread2.src = audioCache[sound];
-            playPromise = audioThread2.play();
-        }
-
-        if (playPromise !== undefined) {
-            playPromise.catch(function(error) {
-                console.log("User didn't interact. Can't play sound!");
-        });
-        }
-    } catch (error) {
-        console.log("Couldn't play sound because of error: " + error);
+  try {
+    var playPromise;
+    if (audioThread1.paused) {
+      audioThread1.src = audioBlobURLs[sound];
+      playPromise = audioThread1.play();
+    } else {
+      audioThread2.src = audioBlobURLs[sound];
+      playPromise = audioThread2.play();
     }
+
+    if (playPromise !== undefined) {
+      playPromise.catch(function (error) {
+        console.log("User didn't interact. Can't play sound!");
+      });
+    }
+  } catch (error) {
+    console.log("Couldn't play sound because of error: " + error);
+  }
 }
 
 // Sleep function
