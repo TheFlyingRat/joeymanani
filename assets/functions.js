2bfd8bd2b12c2129d04ae1cbea0bbbae45afc6d1
TheFlyingRat <joeyspp50@gmail.com>
2023-07-10 13:05:10 +1000

    migrate  getDateDifference from functions to index

--- assets/functions.js
+++ assets/functions.js
@@ -1,35 +1,3 @@
-function getDateDifference(date) {
-    const targetDate = new Date("21 May, 2006");
-    let diffTime = date - targetDate;
-    let diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-    let diffMonths = 0;
-    let diffYears = 0;
-    while (diffDays >= 365) {
-        let year = (new Date(targetDate.getFullYear() + diffYears + 1, 0, 0)).getTime() - (new Date(targetDate.getFullYear() + diffYears, 0, 0)).getTime();
-        let daysInYear = Math.floor(year / (1000 * 60 * 60 * 24));
-        if (diffDays >= daysInYear) {
-        diffYears++;
-        diffDays -= daysInYear;
-        } else {
-        break;
-        }
-    }
-    while (diffDays >= 30) {
-        let month = new Date(targetDate.getFullYear() + diffYears, targetDate.getMonth() + diffMonths + 1, 0).getDate();
-        if (diffDays >= month) {
-        diffMonths++;
-        diffDays -= month;
-        } else {
-        break;
-        }
-    }
-    return {
-        days: diffDays,
-        months: diffMonths,
-        years: diffYears
-    };
-}
-
 // Check if user is a desktop user theflyingrat/joeymanani #9
 function isDesktopUser() {
   // Feature detection: Check for touch support
