32f8b6d55ae52c5cc7f5709c23c5f5635bf4ab12
TheFlyingRat <contact@theflyingrat.com>
2026-02-18 19:16:26 +1100

    Remove old static site files
    Replace with Astro 5 project in subsequent commits.

--- assets/tryEnableSmoothScroll.js
+++
@@ -1,7 +0,0 @@
-// Use smooth scroll if on desktop else don't
-(function tryEnableSmoothScroll(speed, scrollBy) {
-    if (isDesktopUser()) {
-        new smoothScroll(document, speed, scrollBy);
-    }
-})(50, 30);
-// Allow smooth scrolling on the document (IIFE)
